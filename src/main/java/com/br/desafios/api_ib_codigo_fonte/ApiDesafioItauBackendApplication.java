66ff2837e13b46a8ffea2bd56cc0aea3b2085162
D77QB0L1D <almeidadediego@gmail.com>
2025-05-03 15:58:40 -0300

    Commit inicial.

---
+++ src/main/java/com/br/desafios/api_ib_codigo_fonte/ApiDesafioItauBackendApplication.java
@@ -0,0 +1,13 @@
+package com.br.desafios.api_ib_codigo_fonte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDesafioItauBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDesafioItauBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/desafios/api_ib_codigo_fonte/ApiDesafioItauBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.desafios.api_ib_codigo_fonte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDesafioItauBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
